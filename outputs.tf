4c05055e00f08d4be807b142a90dcc2521d24d42
Patrick Ostie <patrick.ostie@stroeer.de>
2022-03-29 23:07:29 +0200

    feat: add outputs to module

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "lambda" {
+  description = "The lambda function resource that has been created."
+  value = aws_lambda_function.authorizer
+}
+
+output "lambda_role" {
+  description = "The attached role of the created lambda function."
+  value = aws_iam_role.authorizer
+}
