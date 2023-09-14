dae1b28d15826a0db308763caa7ef9728cf3b48a
Bastian Pahlke <bastianpahlke@web.de>
2023-09-14 16:24:01 +0200

    use provided.al2 as default runtime

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "suffix" {
 variable "runtime" {
   type        = string
   description = "The runtime that your code needs to run."
-  default     = "go1.x"
+  default     = "provided.al2"
 }
 
 variable "handler" {
