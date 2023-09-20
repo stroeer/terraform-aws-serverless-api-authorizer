978d2fd88d4b82be0d563458567fec4e302d7139
Patrick Ostie <patrick.ostie@stroeer.de>
2023-09-20 14:56:34 +0200

    fix: use >= version constraint for providers

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.2"
+      version = ">= 2.2"
     }
   }
 }
